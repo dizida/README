cc8a2303ff1939240459f8bac2dd77cc2247e694
Sarkissian Sara <dizida1@gmail.com>
2023-09-29 14:16:39 +0200

    commit one testProject

---
+++ testProject/src/main/java/com/mycompany/testproject/TestProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testproject;
+
+/**
+ *
+ * @author sarkissian
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+
+    }
+}
